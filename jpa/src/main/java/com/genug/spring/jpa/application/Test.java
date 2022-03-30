ac6fcedbb5a62f9a1df11fc192ede5a7bd2d91e0
sb.kang <qparanoya@gmail.com>
2022-03-30 10:25:55 +0900

    Created and updated JPA module

---
+++ jpa/src/main/java/com/genug/spring/jpa/application/Test.java
@@ -0,0 +1,27 @@
+package com.genug.spring.jpa.application;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String data;
+
+}

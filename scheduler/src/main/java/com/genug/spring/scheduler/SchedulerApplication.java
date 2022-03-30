e10ed9e4276917cfa3c7237a2b76d683fe1bac5d
sb.kang <qparanoya@gmail.com>
2022-03-30 10:15:03 +0900

    Created Scheduler module

---
+++ scheduler/src/main/java/com/genug/spring/scheduler/SchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.genug.spring.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApplication.class, args);
+    }
+
+}

c9de8711e520ddbffa0701141dd031b86bae1ec3
sb.kang <qparanoya@gmail.com>
2022-04-18 16:41:00 +0900

    Added ORM(JPA) training module.

---
+++ orm/src/main/java/com/genug/spring/OrmApplication.java
@@ -0,0 +1,12 @@
+package com.genug.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrmApplication.class, args);
+    }
+
+}

8ab387bd0a4e3cdfbe382710ba52393bde85bb78
sb.kang <qparanoya@gmail.com>
2022-04-25 16:06:05 +0900

    영속, 비영속 메서드 사용 추가 및 주석 정리

--- orm/src/main/java/com/genug/spring/training/orm/ex01/JpaMain.java
+++ orm/src/main/java/com/genug/spring/training/orm/ex01/JpaMain.java
@@ -18,7 +18,7 @@ public class JpaMain {
 
         // 엔티티 매니저 팩토리 - 생성
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
-        // 엔티티 매니저 - 생성
+        // 엔티티 매니저 - 생성 (영속성 컨텍스트)
         EntityManager em = emf.createEntityManager();
         // 트랜잭션 - 획득
         EntityTransaction tx = em.getTransaction();
@@ -45,9 +45,16 @@ public class JpaMain {
         member.setName("WooJoo");
         member.setAge(36);
 
-        // 등록
+        // 등록 - 1차 캐시 저장
         em.persist(member);
 
+        // 회원 엔티티를 영속석 컨텍스트에서 분리, 준영속 상태
+        em.detach(member);
+
+        // 준영속성 상태의 엔티티를 다시 영속성으로 변경
+        em.merge(member);
+
+        // 메모리의 1차 캐시에서 조회
         Member findMember = em.find(Member.class, id);
         log.info("{}", findMember);
 
@@ -58,11 +65,14 @@ public class JpaMain {
         // SQL - 데이터베이스 테이블을 대상으로 쿼리
         List<Member> members = em
                 .createQuery("select m from Member m", Member.class)
-                //                  ↑ JPQL - 회원 엔티티 객체를 의미, DB의 MEMBER 테이블이 아니다.
+                                            //  ↑ JPQL - 회원 엔티티 객체를 의미, DB의 MEMBER 테이블이 아니다.
                 .getResultList();
         log.info("{}", members);
         // JPQL은 DB 테이블을 전혀 알지 못한다.
 
+        em.clear(); // 영속성 컨테스트에 있는 모든 것이 초기화 - 1차 캐시에 있던 엔티티들이 모두 준영속 상태가 된다.
+
         em.remove(member);
+
     }
 }
